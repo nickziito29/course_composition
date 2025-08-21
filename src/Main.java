ce22364fdc9147573a8d188afe5e9cd76e192fb5
nickziito29 <nbrendo29@gmail.com>
2025-08-21 01:16:56 -0300

    atualizando programa

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import entities.Department;
+import entities.Worker;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.print("Enter worker name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
+        Worker worker =  new Worker(workerName,workerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+
+
+
+
+
+
+
+
+        sc.close();
     }
 }
--- src/entities/enums/WorkerLevel.java
+++ src/entities/enums/WorkerLevel.java
@@ -1,7 +1,7 @@
 package entities.enums;
 
-public enum WorkerLevel {;
-    private int JUNIOR,
-    private int MID_LEVEL,
-    private int SENIOR;
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
 }
